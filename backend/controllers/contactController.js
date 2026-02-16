c3fb6d952e75615064d4287ab687f1319ff1d63d
Suyog <suyog017@gmail.com>
2026-02-16 13:50:45 +0545

    Added nodemailer for recieving emails using google mail

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,5 +1,5 @@
 const Contact = require("../models/contact");
-
+const nodemailer = require("nodemailer");
 //controller to save contact form
 
 const submitContactForm = async(req, res)=>{
@@ -19,6 +19,27 @@ const submitContactForm = async(req, res)=>{
 
         //save to database
         await contact.save();
+
+        //create transporter
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth:{
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        //send email
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER, // I receive it myself
+            subject: "New Contact Message - Smriti Traders",
+            text:`
+                Name: ${name}
+                Phone: ${phone}
+                Message: ${message}
+            `,                
+        });
 
         //send success response
         res.status(201).json({message:"Message sent successfully!"});
